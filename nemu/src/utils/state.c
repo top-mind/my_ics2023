5756c9913c924a65c8c806ecb0f5aef8f76b279b
tracer-ics2023 <tracer@njuics.org>
2023-10-11 21:47:43 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:47:43 up  3:59,  3 users,  load average: 0.21, 0.18, 0.18

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -19,6 +19,7 @@ NEMUState nemu_state = {.state = NEMU_STOP};
 
 int is_exit_status_bad() {
   int good =
-    (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) || (nemu_state.state == NEMU_QUIT);
+    (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) || (nemu_state.state != NEMU_ABORT);
+  // NEMU_QUIT and NEMU_STOP, NEMU_ABORT for error, never hits NEMU_RUNNING.
   return !good;
 }
