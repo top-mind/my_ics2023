1ed980955d4f7fd61956cc37d2809f3d822dd928
tracer-ics2023 <tracer@njuics.org>
2023-10-12 21:48:08 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:48:08 up  3:19,  4 users,  load average: 0.08, 0.05, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -15,7 +15,7 @@
 
 #include <isa.h>
 #include "local-include/reg.h"
-#include "macro.h"
+#include <common.h>
 
 const char *regs[] = {"$0", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "s0", "s1", "a0",
                       "a1", "a2", "a3", "a4", "a5",  "a6",  "a7", "s2", "s3", "s4", "s5",
@@ -23,9 +23,8 @@ const char *regs[] = {"$0", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "s0", "s
 
 void isa_reg_display() {
   for (int i = 0; i < ARRLEN(regs); i++) {
-    
+    // FMT_WORD
   }
-
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) { return 0; }
