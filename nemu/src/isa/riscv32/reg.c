458acd33be31a74d6ff8ea6c0cb5aedff942c4e8
tracer-ics2023 <tracer@njuics.org>
2023-10-12 22:44:17 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:44:17 up  4:16,  4 users,  load average: 0.12, 0.41, 0.39

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -13,6 +13,7 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include <ctype.h>
 #include <isa.h>
 #include "local-include/reg.h"
 #include <common.h>
@@ -22,9 +23,44 @@ const char *regs[] = {"$0", "ra", "sp", "gp", "tp",  "t0",  "t1", "t2", "s0", "s
                       "s6", "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
 
 void isa_reg_display() {
-  for (int i = 0; i < ARRLEN(regs); i++) {
-    // FMT_WORD
+  for (int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++) {
+    printf("%-15s0x%-8x\t0x%x\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
   }
 }
 
-word_t isa_reg_str2val(const char *s, bool *success) { return 0; }
+word_t isa_reg_str2val(const char *s, bool *success) {
+  if (unlikely(s == NULL || s[0] == '\0' || (s[0] == '0' && s[1] != '\0'))) {
+    *success = false;
+    return 0;
+  }
+  if (isdigit(s[0])) {
+    int idx = s[0] & 15;
+    if (s[1] == '\0') {
+      *success = true;
+      return cpu.gpr[idx];
+    }
+    if (unlikely(!isdigit(s[1]))) {
+      *success = false;
+      return 0;
+    }
+    idx = idx * 10 + (s[1] & 15);
+    if (unlikely(idx >= 32 || s[2] != '\0')) {
+      *success = false;
+      return 0;
+    }
+    *success = true;
+    return cpu.gpr[idx];
+  }
+  for (int i = 1; i < 32; i++) {
+    if (strcmp(s, regs[i]) == 0) {
+      *success = true;
+      return cpu.gpr[i];
+    }
+  }
+  if (likely((strcmp(s, "pc") == 0))) {
+    *success = true;
+    return cpu.pc;
+  }
+  *success = false;
+  return 0;
+}
