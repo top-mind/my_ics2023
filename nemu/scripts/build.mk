f282fbcd6e0f4466f477f0d4cebcb3d316232067
tracer-ics2023 <tracer@njuics.org>
2023-10-15 07:35:30 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:35:30 up 10:31,  5 users,  load average: 1.64, 0.85, 0.57

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -22,7 +22,7 @@ CXX := g++
 endif
 LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
-CFLAGS  := -fno-omit-frame-pointer -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
+CFLAGS  := -fsanitize-recover=all -fno-omit-frame-pointer -fno-stack-protector -fsanitize=leak -O0 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
 LDFLAGS := -O2 $(LDFLAGS)
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
