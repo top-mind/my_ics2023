206106d7ee40900ef1796570fc40b96f13580055
221900006-Geng Tiancheng <221900006@smail.nju.edu.cn>
2023-10-15 08:31:51 +0800

    revert, play to figure out bug with GNU readline8.1, suc

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -22,8 +22,8 @@ CXX := g++
 endif
 LD := $(CXX)
 INCLUDES = $(addprefix -I, $(INC_PATH))
-CFLAGS  := -fsanitize-recover=all -fno-omit-frame-pointer -fno-stack-protector -fsanitize=leak -O0 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
-LDFLAGS := -fsanitize-recover=all -fno-omit-frame-pointer -fno-stack-protector -fsanitize=leak -O0 $(LDFLAGS)
+CFLAGS  := -O2 -MMD -Wall -Werror $(INCLUDES) $(CFLAGS)
+LDFLAGS := -O2 $(LDFLAGS)
 
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
