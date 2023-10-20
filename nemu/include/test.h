7fb4eae4b7e96f8a47e37af0489acf8a3a230715
tracer-ics2023 <tracer@njuics.org>
2023-10-20 20:01:19 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:01:19 up  8:08,  6 users,  load average: 0.18, 0.21, 0.22

--- nemu/include/test.h
+++ nemu/include/test.h
@@ -9,11 +9,13 @@ extern word_t expr(char *e, bool *success);
 
 int test_main() {
   uint32_t ans;
+  nemu_state.state = NEMU_QUIT;
   while (2 == scanf("%u%s", &ans, _s)) {
     bool suc = false;
     uint32_t out = expr(_s, &suc);
     if (!suc || ans != out) {
       printf("ans=%u,out=%u,expr=`%s'\n", ans, out, _s);
+      nemu_state.state = NEMU_ABORT;
     }
   }
   return 0;
