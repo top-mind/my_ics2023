15d32750fef828fb0465b352eab0d69b3fed6aa5
tracer-ics2023 <tracer@njuics.org>
2023-10-20 19:47:19 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:47:19 up  7:54,  6 users,  load average: 0.17, 0.19, 0.23

---
+++ nemu/include/test.h
@@ -0,0 +1,21 @@
+// TEST onlu
+#ifndef TEST_H_
+#define TEST_H_
+#include <common.h>
+
+static char _s[65536 + 5];
+
+extern word_t expr(char *e, bool *success);
+
+int test_main(int argc, char *argv[]) {
+  uint32_t ans;
+  while (scanf("%u%s", &ans, _s)) {
+    bool suc = false;
+    uint32_t out = expr(_s, &suc);
+    if (!suc || ans != out) {
+      printf("ans=%u,out=%u,expr=`%s'\n", ans, out, _s);
+    }
+  }
+  return 0;
+}
+#endif
