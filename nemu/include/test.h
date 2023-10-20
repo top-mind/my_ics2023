a20477d06a33725a3bf2df574616fa3a3e2c1afc
tracer-ics2023 <tracer@njuics.org>
2023-10-20 20:09:56 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:09:56 up  8:17,  6 users,  load average: 0.12, 0.21, 0.23

--- nemu/include/test.h
+++ nemu/include/test.h
@@ -3,20 +3,32 @@
 #define TEST_H_
 #include <common.h>
 
-static char _s[65536 + 5];
+static char _s[65536 + 25];
 
 extern word_t expr(char *e, bool *success);
 
 int test_main() {
   uint32_t ans;
   nemu_state.state = NEMU_QUIT;
-  while (2 == scanf("%u%s", &ans, _s)) {
-    bool suc = false;
-    uint32_t out = expr(_s, &suc);
-    if (!suc || ans != out) {
+  // while (2 == scanf("%u%s", &ans, _s)) {
+  //   bool suc = false;
+  //   uint32_t out = expr(_s, &suc);
+  //   if (!suc || ans != out) {
+  //     printf("ans=%u,out=%u,expr=`%s'\n", ans, out, _s);
+  //     nemu_state.state = NEMU_ABORT;
+  //   }
+  // }
+  while (scanf("%u%ss", &ans, _s) == 1) {
+    size_t len = strlen(_s);
+    uint32_t out;
+    sprintf(_s + len, "==%u", ans);
+    bool suc;
+    if ((out = expr(_s, &suc)) != 1) {
+
       printf("ans=%u,out=%u,expr=`%s'\n", ans, out, _s);
       nemu_state.state = NEMU_ABORT;
     }
+
   }
   return 0;
 }
