7ad1828b7caca3b685f5d9b93626f1c5c3ad83db
tracer-ics2023 <tracer@njuics.org>
2023-10-27 00:04:13 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:04:13 up  2:59,  3 users,  load average: 0.31, 0.40, 0.59

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -12,6 +12,7 @@
  * * See the Mulan PSL v2 for more details.
  ***************************************************************************************/
 
+#include "cpu/decode.h"
 #include "memory/paddr.h"
 #include <isa.h>
 #include <errno.h>
@@ -233,13 +234,17 @@ static int compile_token(int l, int r) {
     return 0;
   }
   if (l == r) {
-    if (tokens[l].type == TK_NUM) {
-      p_rpn[nr_rpn].type = tokens[l].type;
-      p_rpn[nr_rpn].numconstant = tokens[l].numconstant;
-      nr_rpn++;
-    } else {
-      printf("Syntax error near `%s'\n", l + 1 < nr_token ? p_expr + tokens[l + 1].position : "");
-      return 0;
+    p_rpn[nr_rpn].type = tokens[l].type;
+    switch (tokens[l].type) {
+      case TK_NUM:
+        p_rpn[nr_rpn].numconstant = tokens[l].numconstant;
+        break;
+      case TK_DOLLAR:
+        p_rpn[nr_rpn].preg = tokens[l].preg;
+        break;
+      default:
+        printf("Syntax error near `%s'\n", l + 1 < nr_token ? p_expr + tokens[l + 1].position : "");
+        return 0;
     }
   } else {
     if (tokens[r].type == ')' && tokens[r].lbmatch == l) return compile_token(l + 1, r - 1);
