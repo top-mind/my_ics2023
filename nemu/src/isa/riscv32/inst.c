f0e937d9a9c923b78444f91f4de16501de5049bf
tracer-ics2023 <tracer@njuics.org>
2023-10-31 09:49:51 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:49:51 up 15 min,  4 users,  load average: 0.26, 0.45, 0.38

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -115,7 +115,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("", shift, IS,);
   INSTPAT("", a_r, R, );
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall, N, INV(s->pc));
-  printf("fall here");
+  printf("fall here\n");
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak, N,
           NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv, N, INV(s->pc));
