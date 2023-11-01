926c738d862751f1d1f7616853a16221ce40563d
tracer-ics2023 <tracer@njuics.org>
2023-11-02 03:06:18 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:06:18 up 1 day,  2:48,  6 users,  load average: 0.13, 0.11, 0.06

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -11,6 +11,12 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
+LBRACE=(
+RBRACE=)
+LDFLAGS += $(shell echo -n '$(nemu_margs)\0' | xxd -p | sed -r 's/$(LBRACE)..$(RBRACE)/BYTE$(LBRACE)0x\1$(RBRACE) /g'> build/margs.ld)
+all:
+	# $(FLAG)
+	@ cat out.txt
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
