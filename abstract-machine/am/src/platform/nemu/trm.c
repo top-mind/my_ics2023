9511a7506222afed12257c214cdb860649167100
tracer-ics2023 <tracer@njuics.org>
2023-11-08 16:31:49 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:31:49 up 1 day,  2:18,  3 users,  load average: 0.41, 0.58, 1.26

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -8,8 +8,8 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
-extern const char *_nemu_mainargs;
-// static const char mainargs[] = MAINARGS;
+// extern const char *_nemu_mainargs;
+static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
   outb(SERIAL_PORT, ch);
@@ -17,13 +17,12 @@ void putch(char ch) {
 
 void halt(int code) {
   nemu_trap(code);
-
   // should not reach here
   while (1);
 }
 
 void _trm_init() {
-  // int ret = main(mainargs);
-  int ret = main(_nemu_mainargs);
+  int ret = main(mainargs);
+  // int ret = main(_nemu_mainargs);
   halt(ret);
 }
