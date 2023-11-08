54a394467f9eeafbf8321da8da52ca8db9c0c44a
tracer-ics2023 <tracer@njuics.org>
2023-11-09 03:31:30 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:31:30 up 1 day, 13:18,  4 users,  load average: 0.48, 0.32, 0.29

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -44,6 +44,7 @@ static void out_of_bound(paddr_t addr) {
   printf(ANSI_FMT("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR
         "] at pc = " FMT_WORD, ANSI_FG_RED),
         addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
+  puts("");
 }
 
 void init_mem() {
