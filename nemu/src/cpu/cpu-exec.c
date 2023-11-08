471f392dd312e8610f76e723039c2ca6facc69fd
tracer-ics2023 <tracer@njuics.org>
2023-11-09 02:11:40 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:11:40 up 1 day, 11:58,  4 users,  load average: 0.50, 0.30, 0.31

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -29,6 +29,9 @@ CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
+#ifdef C
+char g_iring_buf[][sizeof ((Decode *)0)->logbuf] = {};
+#endif
 
 void device_update();
 
