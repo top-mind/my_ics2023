1405817cd61201d1f09f437c6b1222feeac00545
tracer-ics2023 <tracer@njuics.org>
2023-11-11 14:24:34 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:24:34 up 13:13,  4 users,  load average: 0.04, 0.07, 0.08

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -73,6 +73,11 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+#ifdef CONFIG_MTRACE
+  if (addr >= CONFIG_MTRACE_START && addr < CONFIG_MTRACE_END) {
+    log_write("@ [" FMT_PADDR "] w " FMT_PADDR " %d %*" PRIx32, cpu.pc, addr, len, len * 2, data);
+  }
+#endif
   if (likely(in_pmem(addr))) {
     pmem_write(addr, len, data);
     return;
