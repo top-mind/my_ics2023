694023d28ab20c02ef5328acb749d4e179a4611d
tracer-ics2023 <tracer@njuics.org>
2023-11-14 12:57:11 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:57:11 up 4 min,  3 users,  load average: 0.23, 0.33, 0.17

--- nemu/src/monitor/sdb/elf.c
+++ nemu/src/monitor/sdb/elf.c
@@ -89,6 +89,7 @@ void init_addelf(char *filename) {
     }
   }
   qsort(funcs, sizeof(func), nr_func, compfunc);
+  printf("%zu\n", nr_func);
   for (int i = 0; i < nr_func; i++)
     printf("name='%s', %#lx, %ld\n", funcs[i].name, (long)funcs[i].addr, (long)funcs[i].size);
   fclose(f);
