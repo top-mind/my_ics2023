14d9a03126c53acfec3874bdf5ce459204208c62
tracer-ics2023 <tracer@njuics.org>
2023-11-15 03:19:58 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:19:58 up  2:58,  3 users,  load average: 0.14, 0.11, 0.14

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -12,7 +12,6 @@
  * * See the Mulan PSL v2 for more details.
  ***************************************************************************************/
 
-#include "cpu/decode.h"
 #include "memory/paddr.h"
 #include "sdb.h"
 #include <isa.h>
