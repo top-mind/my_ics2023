6fe46be112e7633a23bc680bb1e0e57e2ab27d21
tracer-ics2023 <tracer@njuics.org>
2023-11-18 03:47:21 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:47:21 up 1 day,  4:43,  5 users,  load average: 0.17, 0.28, 0.30

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,8 +5,8 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint32_t high = inl(AM_TIMER_RTC + 4);
-  uint32_t low = inl(AM_TIMER_RTC);
+  uint32_t high = inl(RTC_ADDR + 4);
+  uint32_t low = inl(RTC_ADDR);
   uptime->us = high;
   uptime->us = (uptime->us << 32) | low;
 }
