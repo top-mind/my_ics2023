0c21547d204582b6a073808ba61c52511469a13f
tracer-ics2023 <tracer@njuics.org>
2023-11-18 05:34:31 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:34:31 up 1 day,  6:30,  5 users,  load average: 0.22, 0.28, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -27,10 +27,9 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
   if (W == 0)
     panic("Please call `AM_GPUU_CONFIG` first!");
-  if (W!=400) panic("");
   for (int i = x; i < x + w; i++)
     for (int j = y; j < y + h; j++)
-      outb(FB_ADDR + (i + j * W) * 4, p[i + j * w]);
+      outb(FB_ADDR + (i * W + j)  * 4, p[i * w + j]);
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
