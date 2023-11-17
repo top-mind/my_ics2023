33963a6084773191ec93dfcad4cf5c218c894df2
tracer-ics2023 <tracer@njuics.org>
2023-11-18 05:32:06 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:32:06 up 1 day,  6:28,  5 users,  load average: 0.42, 0.27, 0.36

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -6,15 +6,30 @@
 void __am_gpu_init() {
 }
 
+static int W = 0, H = 0;
+
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  if (W == 0) {
+    uint32_t screen_info = inl(VGACTL_ADDR);
+    W = screen_info >> 16;
+    H = screen_info & 0xffff;
+  }
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = W, .height = H,
+    .vmemsz = W * H * 4,
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  // AM_DEVREG(11, GPU_FBDRAW,   WR, int x, y; void *pixels; int w, h; bool sync);
+  uint32_t *p = ctl->pixels;
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (W == 0)
+    panic("Please call `AM_GPUU_CONFIG` first!");
+  for (int i = x; i < x + w; i++)
+    for (int j = y; j < y + h; j++)
+      outb(FB_ADDR + (i + j * W) * 4, p[i + j * w]);
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
