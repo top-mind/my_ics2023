6a8b4aa93650cd14ddfff87ca869fb6d4e4ca120
tracer-ics2023 <tracer@njuics.org>
2023-11-18 18:21:13 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:21:13 up  2:38,  3 users,  load average: 0.58, 0.34, 0.32

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -12,15 +12,35 @@ void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+  cfg->bufsize = 0x10000;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR, ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  uintptr_t start = (uintptr_t)ctl->buf.start;
+  uintptr_t len = (uintptr_t)ctl->buf.end - (uintptr_t)ctl->buf.end;
+  // aligned write
+  if (len & 1) {
+    len = len - 1;
+    outb(AUDIO_SBUF_SIZE_ADDR + len, *(uint8_t *)(start + len));
+  }
+  if (len & 3) {
+    len = len - 2;
+    outw(AUDIO_SBUF_SIZE_ADDR + len, *(uint16_t *)(start + len));
+  }
+  if (len == 0)
+    return;
+  for (int i = len - 1; i >= 0; i -= 4) {
+    outl(AUDIO_SBUF_SIZE_ADDR + i, *(uint32_t *)(start + i));
+  }
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -13,6 +13,7 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include <SDL2/SDL_audio.h>
 #include <common.h>
 #include <device/map.h>
 #include <SDL2/SDL.h>
@@ -22,7 +23,63 @@ enum { reg_freq, reg_channels, reg_samples, reg_sbuf_size, reg_init, reg_count, 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
-static void audio_io_handler(uint32_t offset, int len, bool is_write) {}
+static int queue_head = 0, queue_tail = 0;
+static bool valid_count = false;
+static uint8_t sdl_silence = 0;
+static uint32_t sdl_size = 0;
+
+static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  switch(offset) {
+    case reg_freq:
+      break;
+    case reg_channels:
+      break;
+    case reg_samples:
+      break;
+    case reg_sbuf_size:
+      assert(!is_write);
+      break;
+    case reg_init:
+      if (is_write) {
+        // init SDL audio
+        SDL_AudioSpec want = {
+          .callback = NULL,
+          .channels = audio_base[reg_channels],
+          .format = AUDIO_S16SYS,
+          .freq = audio_base[reg_freq],
+          .samples = audio_base[reg_samples],
+          .userdata = NULL,
+        };
+        SDL_OpenAudio(&want, NULL);
+        sdl_silence = want.silence;
+        sdl_size = want.size;
+        valid_count = true;
+      }
+      break;
+    case reg_count:
+      assert(!is_write);
+      assert(valid_count);
+      int used = (queue_tail + CONFIG_SB_SIZE - queue_head) % CONFIG_SB_SIZE;
+      audio_base[reg_count] = CONFIG_SB_SIZE - used - 1;
+      break;
+    default:
+      assert(0);
+  }
+}
+
+static void audio_sbuf_handler(uint32_t offset, int len, bool is_write) {
+  assert(is_write);
+  if (offset == 0) {
+    valid_count = true;
+    SDL_QueueAudio(1, sbuf, queue_tail ? queue_tail : len);
+    queue_tail = 0;
+  } else {
+    if (valid_count) {
+      queue_tail = offset + len;
+      valid_count = false;
+    }
+  }
+}
 
 void init_audio() {
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
@@ -32,7 +89,9 @@ void init_audio() {
 #else
   add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
 #endif
-
+  memset(audio_base, 0, space_size);
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
-  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+  add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, audio_sbuf_handler);
+  memset(sbuf, 0, CONFIG_SB_SIZE);
 }
