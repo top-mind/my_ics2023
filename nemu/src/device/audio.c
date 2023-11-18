90d4ec2a1bed97e65db95f3771dfbed2f9e9def5
tracer-ics2023 <tracer@njuics.org>
2023-11-19 00:28:59 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:28:59 up  4:55,  3 users,  load average: 0.80, 1.15, 1.28

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -96,7 +96,7 @@ void init_audio() {
   add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
 #endif
   memset(audio_base, 0, space_size);
-  audio_base[reg_sbuf_size] = 4;
+  audio_base[reg_sbuf_size] = 368;
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, audio_sbuf_handler);
   memset(sbuf, 0, CONFIG_SB_SIZE);
