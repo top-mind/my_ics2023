d343a35f284a86fc6c3126eaa6caaa304597f74c
tracer-ics2023 <tracer@njuics.org>
2023-11-19 00:30:33 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:30:33 up  4:56,  3 users,  load average: 0.90, 1.08, 1.24

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -59,10 +59,9 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
     case reg_count:
       assert(!is_write);
       assert(is_audio_sbuf_idle);
-      // uint32_t used = 2652;
-      int used = SDL_GetQueuedAudioSize(1);
-      printf("%d\n", used);
-      audio_base[reg_count] = used;
+      // uint32_t used = SDL_GetQueuedAudioSize(1);
+      // printf("%d\n", used);
+      audio_base[reg_count] = 2820 - 100;
       break;
     default:
       printf("%d\n", offset);
@@ -97,7 +96,7 @@ void init_audio() {
   add_mmio_map("audio", CONFIG_AUDIO_CTL_MMIO, audio_base, space_size, audio_io_handler);
 #endif
   memset(audio_base, 0, space_size);
-  audio_base[reg_sbuf_size] = 736;
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, audio_sbuf_handler);
   memset(sbuf, 0, CONFIG_SB_SIZE);
