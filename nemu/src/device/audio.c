a12bcd698716238281064c0830274f46d6aaffee
tracer-ics2023 <tracer@njuics.org>
2023-11-19 07:47:33 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:47:33 up 12:13,  3 users,  load average: 1.11, 0.95, 0.63

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -28,7 +28,7 @@ static int block_size;
 static uint32_t count_old;
 static int upd_delay;
 
-#define CONFIG_DELAY 0
+#define CONFIG_DELAY 10
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
   switch(offset / sizeof(uint32_t)) {
