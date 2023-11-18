a8e15024c731c05efa92ba9811506f34bc176213
tracer-ics2023 <tracer@njuics.org>
2023-11-19 00:00:04 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:00:04 up  4:26,  3 users,  load average: 1.30, 0.96, 1.26

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -31,7 +31,7 @@ void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
   uintptr_t start = (uintptr_t)ctl->buf.start;
   intptr_t len = (uintptr_t)ctl->buf.end - (uintptr_t)ctl->buf.start;
-  printf("%p %d\n", start, len);
+  printf("%p %p\n", ctl->buf.start, ctl->buf.end);
   if (len < 0)
     return;
   for (int i = 0; i < len; i += 4) {
