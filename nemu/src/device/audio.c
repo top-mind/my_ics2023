af236ac7e58dc47b0cbc49c210745893e973f0dd
tracer-ics2023 <tracer@njuics.org>
2023-11-19 00:24:37 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:24:37 up  4:51,  3 users,  load average: 1.48, 1.64, 1.44

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -48,7 +48,6 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
           .userdata = NULL,
         };
         printf("SDL audio: freq = %d, channels = %d, samples = %d\n", want.freq, want.channels, want.samples);
-        want.samples = 2048;
         if (SDL_OpenAudio(&want, NULL)) {
           printf("SDL audio: %s\n", SDL_GetError());
           assert(0);
@@ -60,7 +59,7 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
     case reg_count:
       assert(!is_write);
       assert(is_audio_sbuf_idle);
-      uint32_t used = SDL_GetQueuedAudioSize(1);
+      uint32_t used = 2400;
       audio_base[reg_count] = used;
       break;
     default:
