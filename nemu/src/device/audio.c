3749734ad5a114856967b6693c38d7a0afaf8439
tracer-ics2023 <tracer@njuics.org>
2023-11-19 01:10:50 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:10:50 up  5:37,  3 users,  load average: 0.75, 0.66, 0.79

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -93,7 +93,7 @@ static void audio_sbuf_handler(uint32_t offset, int len, bool is_write) {
     is_audio_sbuf_idle = true;
     if (slow_cnt == CONFIG_SLOW_RATE - 1) {
       slow_cnt = 0;
-      if (CONFIG_SLOW_RATE * SDL_GetQueuedAudioSize(1) > 2820)
+      if (CONFIG_SLOW_RATE * (4 + SDL_GetQueuedAudioSize(1)) > 2820)
         return;
       if (0 != SDL_QueueAudio(1, sbuf, block_size ?: len)) printf("SDL: %s\n", SDL_GetError());
     } else {
