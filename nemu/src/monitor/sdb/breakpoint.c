2e49b6be95645b3d009c10e5f83f7b3838a4c062
tracer-ics2023 <tracer@njuics.org>
2023-11-24 02:42:45 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:42:45 up 17:00,  3 users,  load average: 0.35, 0.33, 0.25

--- nemu/src/monitor/sdb/breakpoint.c
+++ nemu/src/monitor/sdb/breakpoint.c
@@ -7,9 +7,14 @@ static int end;
 unsigned cnt_bp;
 BP bp_pool[NR_BP];
 
-static inline void new_breakpoint(char *s) {
+static inline bool new_breakpoint(char *s) {
   BP *p = &bp_pool[end];
-  paddr_t addr = 0; // TODO
+  paddr_t addr = 0x80000000; // TODO
+  if (!in_pmem(addr)) {
+    printf("I found %s at " FMT_PADDR ", but address out of range\n"
+           "May symbol table unmatch?\n",
+           s, addr);
+  }
   p->b.addr = addr;
   p->b.raw_instr = inst_fetch(&addr, sizeof p->b.raw_instr);
   // clang-format off
@@ -20,9 +25,10 @@ static inline void new_breakpoint(char *s) {
                   ))));
   // clang-format on
   host_write(guest_to_host(addr), sizeof p->b.raw_instr, data);
+  return true;
 }
 
-static inline void new_watchpoint(char *s) {
+static inline bool new_watchpoint(char *s) {
   BP *p = &bp_pool[end];
   // p->w->rpn; TODO
   // p->w->nr_rpn;
@@ -33,18 +39,18 @@ static inline void new_watchpoint(char *s) {
     ;
   if (cur != -1) bp_pool[cur].next = end;
   p->w.prev = cur;
+  return true;
 }
 
 int new_bp(char *s, bool is_watchpoint) {
   if (end == -1) return -1;
+  bool suc = is_watchpoint ? new_watchpoint(s) : new_breakpoint(s);
+  if (!suc)
+    return -1;
   bp_pool[end].num = ++cnt_bp;
   bp_pool[end].is_watchpoint = is_watchpoint;
   bp_pool[end].hint = s;
   bp_pool[end].hit = 0;
-  if (is_watchpoint)
-    new_watchpoint(s);
-  else
-    new_breakpoint(s);
   end = bp_pool[end].next;
   return cnt_bp;
 }
