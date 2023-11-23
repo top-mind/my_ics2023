be68226c3591f847178d2d4001125345a1b25785
tracer-ics2023 <tracer@njuics.org>
2023-11-23 15:32:16 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:32:16 up  5:50,  3 users,  load average: 0.38, 0.19, 0.09

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -55,30 +55,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 
 #ifdef CONFIG_ITRACE
 #include <memory/paddr.h> // in_pmem
-static char *trace_disassemble(Decode *s) {
-  const int nrbuf = 128;
-  char *p, *buf = p = malloc(nrbuf);
-  p += snprintf(p, nrbuf, FMT_WORD ":", s->pc);
-  int ilen = s->snpc - s->pc;
-  int i;
-  uint8_t *inst = (uint8_t *)&s->isa.inst.val;
-  for (i = ilen - 1; i >= 0; i--) { p += snprintf(p, 4, " %02x", inst[i]); }
-  int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
-  int space_len = ilen_max - ilen;
-  if (space_len < 0) space_len = 0;
-  space_len = space_len * 3 + 1;
-  memset(p, ' ', space_len);
-  p += space_len;
-
-#ifndef CONFIG_ISA_loongarch32r
-  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-  disassemble(p, buf + nrbuf - p, MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc),
-              (uint8_t *)&s->isa.inst.val, ilen);
-#else
-  p[0] = '\0'; // the upstream llvm does not support loongarch32r
-#endif
-  return buf;
-}
 #endif
 
 static void do_itrace(Decode *s) {
@@ -90,6 +66,7 @@ static void do_itrace(Decode *s) {
       free(assem);
     }, );
 }
+
 
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
--- nemu/src/cpu/trace.h
+++ nemu/src/cpu/trace.h
@@ -1,8 +1,32 @@
 #ifndef __TRACE_H__
 #include <cpu/decode.h>
 
-static void do_itrace(Decode *);
 static void do_iqtrace(Decode *s) {}
 static void do_ftrace(Decode *s) { MUXDEF(CONFIG_FTRACE, isa_ras_update(s),); }
+static void do_itrace(Decode *);
 
+static char *trace_disassemble(Decode *s) {
+  const int nrbuf = 128;
+  char *p, *buf = p = malloc(nrbuf);
+  p += snprintf(p, nrbuf, FMT_WORD ":", s->pc);
+  int ilen = s->snpc - s->pc;
+  int i;
+  uint8_t *inst = (uint8_t *)&s->isa.inst.val;
+  for (i = ilen - 1; i >= 0; i--) { p += snprintf(p, 4, " %02x", inst[i]); }
+  int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
+  int space_len = ilen_max - ilen;
+  if (space_len < 0) space_len = 0;
+  space_len = space_len * 3 + 1;
+  memset(p, ' ', space_len);
+  p += space_len;
+
+#ifndef CONFIG_ISA_loongarch32r
+  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+  disassemble(p, buf + nrbuf - p, MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc),
+              (uint8_t *)&s->isa.inst.val, ilen);
+#else
+  p[0] = '\0'; // the upstream llvm does not support loongarch32r
+#endif
+  return buf;
+}
 #endif
