0daada8f0075ece52b3b33b5fad912ce244c9f20
tracer-ics2023 <tracer@njuics.org>
2023-11-24 02:53:57 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:53:57 up 17:12,  3 users,  load average: 0.19, 0.18, 0.19

--- nemu/src/monitor/sdb/breakpoint.c
+++ nemu/src/monitor/sdb/breakpoint.c
@@ -11,9 +11,9 @@ static inline bool new_breakpoint(char *s) {
   BP *p = &bp_pool[end];
   paddr_t addr = 0x7fffffff; // TODO
   if (!in_pmem(addr)) {
-    printf("I found %s at " FMT_PADDR ", but address out of range."
+    printf("I found %s at " FMT_PADDR ", but address out of range. "
            "May symbol table unmatch?\n"
-           "Fail to insert breakpoint",
+           "Fail to insert breakpoint\n",
            s, addr);
     return false;
   }
