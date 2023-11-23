d425e7da10b434c5acef7a3cecf3e2c32f4e0ca5
tracer-ics2023 <tracer@njuics.org>
2023-11-23 18:04:24 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:04:24 up  8:22,  3 users,  load average: 0.05, 0.26, 0.32

--- nemu/src/cpu/trace.h
+++ nemu/src/cpu/trace.h
@@ -1,13 +1,14 @@
 #ifndef __TRACE_H__
 #define __TRACE_H__
-#include "cpu/decode.h"
+#include <common.h>
+#include <cpu/decode.h>
 
 #define MAX_INST_TO_PRINT 10
 static bool g_print_step = false;
 
 #ifdef CONFIG_TRACE
 char *trace_disassemble(Decode *s);
-// Decode iqueue_buf[];
+Decode iqueue_buf[CONFIG_NR_IQUEUE];
 
 static inline void do_iqtrace(Decode *s) {}
 static inline void do_ftrace(Decode *s) { MUXDEF(CONFIG_FTRACE, isa_ras_update(s), ); }
