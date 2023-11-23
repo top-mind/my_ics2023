05b8e5abfeaaf9c9c1dcbc77979446c1d26d2dd1
tracer-ics2023 <tracer@njuics.org>
2023-11-24 01:21:04 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:21:04 up 15:39,  3 users,  load average: 0.12, 0.11, 0.05

--- nemu/src/monitor/sdb/sdb.h
+++ nemu/src/monitor/sdb/sdb.h
@@ -69,8 +69,8 @@ typedef struct __attribute__((packed)) _breakpoint {
     } b;
     struct {
       rpn_t *rpn;
-      int nr_rpn;
       eval_t old_value;
+      int nr_rpn;
     } w;
   };
 } BP;
