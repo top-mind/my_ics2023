bffe9df4b8433311da246c764691747dcc6927bf
tracer-ics2023 <tracer@njuics.org>
2023-11-24 01:19:53 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:19:53 up 15:38,  3 users,  load average: 0.13, 0.11, 0.04

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,7 +55,7 @@ static int cmd_c(char *args) {
 }
 
 static int cmd_q(char *args) {
-  printf("%zd\n", sizeof(((BP *)0)->w.old_value));
+  printf("%zd\n", sizeof(((BP *)0)->w));
   nemu_state.state = NEMU_QUIT;
   return -1;
 }
