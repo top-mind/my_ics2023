348608268e1ba77098343a0dfeb1f8e0b28b73da
tracer-ics2023 <tracer@njuics.org>
2023-12-02 02:41:19 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:41:19 up 1 day, 13:40,  3 users,  load average: 0.24, 0.42, 0.38

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -220,33 +220,22 @@ static int cmd_si(char *args) {
   return 0;
 }
 
-// #ifdef CONFIG_ITRACE
-// #include <cpu/decode.h>
-// #endif
 static int cmd_info(char *args) {
   char *arg = strtok(NULL, " ");
   if (arg == NULL) {
     puts("info r  \tdisplay registers");
     puts("info w  \tstatus watchpoints");
+    puts("info h  \thistrory intructions");
   } else {
     if (strcmp(arg, "r") == 0) {
       isa_reg_display();
-      // #ifdef CONFIG_ITRACE
-      //       printf("%-15s" FMT_WORD, "pc", cpu.pc);
-      //       fflush(stdout); // llvm::outs() aka raw_string write doesn't sync stdout
-      //       Decode s = {.pc = cpu.pc, .snpc = cpu.pc};
-      //       isa_fetch_decode(&s);
-      //       void disassemblePrint(uint64_t pc, uint8_t * code, int nbyte);
-      //       disassemblePrint(MUXDEF(CONFIG_ISA_x86, s.snpc, s.pc), (uint8_t *)&s.isa.instr.val,
-      //                        s.snpc - s.pc);
-      //       putchar('\n');
-      // #else
       printf("%-15s0x%-" MUXDEF(CONFIG_ISA64, "16l", "8") "x\n", "pc", cpu.pc);
       // #endif
     } else if (strcmp(arg, "w") == 0) {
       print_wp();
-    } else if (strcmp(arg, "f") == 0) {
-      // not imp
+    } else if (strcmp(arg, "h") == 0) {
+      MUXDEF(CONFIG_IQUEUE, void irtrace_print();
+             irtrace_print(), puts("Please enable iring tracer"));
     } else {
       printf("Unknown subcommand %s, try help info.\n", arg);
     }
