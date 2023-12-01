2badd1693b27cd34e449fd78b1cecaafc0c19ca5
tracer-ics2023 <tracer@njuics.org>
2023-12-02 01:45:33 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:45:33 up 1 day, 12:44,  3 users,  load average: 0.18, 0.26, 0.59

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -25,18 +25,9 @@ static uint64_t g_timer = 0; // unit: us
 
 void device_update();
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
-#ifdef CONFIG_TRACE
-  do_itrace(_this);
-  do_iqueue(_this);
-  do_ftrace(_this);
-#endif
 
 #ifndef CONFIG_TARGET_AM
-  bool is_watchpoint_hit();
-  if (is_watchpoint_hit()) {
-    if (nemu_state.state == NEMU_RUNNING)
-      nemu_state.state = NEMU_STOP;
-  }
+  // TODO stop if hit a breakpoint
 #endif
 
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
@@ -60,33 +51,6 @@ static void execute(uint64_t n) {
   }
 }
 
-#if defined(CONFIG_ITRACE) || defined(CONFIG_IQUEUE)
-char *trace_disassemble(Decode *s) {
-  const int nrbuf = 128;
-  char *p, *buf = p = malloc(nrbuf);
-  p += snprintf(p, nrbuf, FMT_WORD ":", s->pc);
-  int ilen = s->snpc - s->pc;
-  int i;
-  uint8_t *inst = (uint8_t *)&s->isa.inst.val;
-  for (i = ilen - 1; i >= 0; i--) { p += snprintf(p, 4, " %02x", inst[i]); }
-  int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
-  int space_len = ilen_max - ilen;
-  if (space_len < 0) space_len = 0;
-  space_len = space_len * 3 + 1;
-  memset(p, ' ', space_len);
-  p += space_len;
-
-#ifndef CONFIG_ISA_loongarch32r
-  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-  disassemble(p, buf + nrbuf - p, MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc),
-              (uint8_t *)&s->isa.inst.val, ilen);
-#else
-  p[0] = '\0'; // the upstream llvm does not support loongarch32r
-#endif
-  return buf;
-}
-#endif
-
 static void statistic() {
   IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
 #define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
@@ -100,13 +64,13 @@ static void statistic() {
 
 void print_fail_msg() {
   isa_reg_display();
-  trace_show_message_error();
+  // trace_show_message_error();
   statistic();
 }
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
-  g_print_step = (n < MAX_INST_TO_PRINT);
+  // TODO tell the trace system N.
   switch (nemu_state.state) {
     case NEMU_END:
     case NEMU_ABORT:
--- nemu/src/cpu/trace.h
+++ nemu/src/cpu/trace.h
@@ -3,54 +3,6 @@
 #include <cpu/decode.h>
 
 #define MAX_INST_TO_PRINT 10
-static bool g_print_step = false;
 
-#ifdef CONFIG_TRACE
-char *trace_disassemble(Decode *s);
-#ifdef CONFIG_IQUEUE
-Decode iqueue_buf[CONFIG_NR_IQUEUE];
-static size_t iqueue_end = 0;
-static bool iquque_wrap = false;
-#endif
-
-static inline void do_iqueue(Decode *s) {
-#ifdef CONFIG_IQUEUE
-  memcpy(iqueue_buf + iqueue_end++, s, sizeof(Decode));
-  if (iqueue_end == CONFIG_NR_IQUEUE) {
-    iqueue_end = 0;
-    iquque_wrap = 1;
-  }
-  Log("1 %zd %d", iqueue_end, iquque_wrap);
-#endif
-}
-static inline void do_ftrace(Decode *s) { MUXDEF(CONFIG_FTRACE, isa_ras_update(s), ); }
-static inline void do_itrace(Decode *s) {
-  MUXDEF(
-    CONFIG_ITRACE, if (CONFIG_ITRACE_COND || g_print_step) {
-      char *assem = trace_disassemble(s);
-      if (CONFIG_ITRACE_COND) log_write("%s\n", assem);
-      if (g_print_step) puts(assem);
-      free(assem);
-    }, );
-}
-#endif
-
-void trace_show_message_error() {
-#ifdef CONFIG_IQUEUE
-#define disasm_print                                 \
-  do {                                               \
-    char *assem = trace_disassemble(iqueue_buf + i); \
-    puts(assem);                                     \
-    free(assem);                                     \
-  } while (0)
-  size_t i;
-  if (iquque_wrap)
-    for (i = iqueue_end; i < CONFIG_NR_IQUEUE; i++)
-      disasm_print;
-  for (i = 0; i < iqueue_end; i++)
-    disasm_print;
-#undef disasm_print
-#endif
-}
 
 #endif
