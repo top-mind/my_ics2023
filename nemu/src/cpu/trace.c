1d79a44c720108e898788aacdec353f5cedcd902
tracer-ics2023 <tracer@njuics.org>
2023-12-02 08:47:13 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:47:13 up 1 day, 19:46,  3 users,  load average: 0.16, 0.18, 0.20

--- nemu/src/cpu/trace.c
+++ nemu/src/cpu/trace.c
@@ -137,12 +137,12 @@ void ftrace_push(vaddr_t _pc, vaddr_t dnpc) {
   bool need_minus_nr_repeat, need_print_old, need_print_lbrace, need_print_new;
   need_print_old = ras_tailcall && ras_nr_repeat > 1;
   need_minus_nr_repeat = need_print_lbrace = ras_tailcall;
-  need_print_new = ras_tailcall || dnpc == ras_lastpc || ras_nr_repeat == 0;
+  need_print_new = ras_tailcall || dnpc != ras_lastpc || ras_nr_repeat == 0;
   // The last condition is necessary. Consider lastpc being initially a function entry.
   // That is, the first call is ((void (*)())0)()
   // flush
   if (need_minus_nr_repeat) ras_nr_repeat--;
-  if (!need_print_new)
+  if (need_print_new)
     ftrace_flush();
   // print old
   if (need_print_old)
