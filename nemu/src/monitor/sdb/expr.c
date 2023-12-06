7b4caead5ab959b6b716d98c20cbcc0ab727a455
tracer-ics2023 <tracer@njuics.org>
2023-12-06 11:58:30 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:58:30 up 45 min,  3 users,  load average: 0.32, 0.20, 0.21

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -107,7 +107,7 @@ static struct rule {
   {"\\(", '('},       // lbrace
   {"\\)", ')'},       // rbrace
   {"\\$[a-z0-9$]+", TK_DOLLAR},
-  {"[[:alnum:]]", TK_SYM}
+  {"[::alnum::]", TK_SYM}
 };
 
 #define NR_REGEX ARRLEN(rules)
