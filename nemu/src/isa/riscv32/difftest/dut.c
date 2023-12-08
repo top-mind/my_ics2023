6c5d9d8b7f2fe485c7a7c2f4f1f54d7737f405ba
tracer-ics2023 <tracer@njuics.org>
2023-12-09 00:21:35 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:21:35 up 55 min,  3 users,  load average: 0.23, 0.52, 0.65

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -21,12 +21,15 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for (int i = 0; i < ARRLEN(((CPU_state *)0)->gpr); ++i) {
     if (ref_r->gpr[i] != cpu.gpr[i]) return false;
   }
-  if (ref_r->mepc != cpu.mepc ||
-      ref_r->mcause != cpu.mcause ||
-      ref_r->mtvec != cpu.mtvec ||
-      ref_r->mstatus != cpu.mstatus)
-    return false;
-  return true;
+  if (ref_r->mepc != cpu.mepc)
+    printf("mepc: 0x%x, 0x%x\n", ref_r->mepc, cpu.mepc);
+  if (ref_r->mcause != cpu.mcause)
+    printf("mcause: 0x%x, 0x%x\n", ref_r->mcause, cpu.mcause);
+  if (ref_r->mtvec != cpu.mtvec)
+    printf("mtvec: 0x%x, 0x%x\n", ref_r->mtvec, cpu.mtvec);
+  if (ref_r->mstatus != cpu.mstatus)
+    printf("mstatus: 0x%x, 0x%x\n", ref_r->mstatus, cpu.mstatus);
+  return ref_r->pc == pc;
 }
 
 void isa_difftest_attach() {}
