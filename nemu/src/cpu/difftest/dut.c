e2376e824eab90b1ed2d4cbf567ab4bed88b754b
tracer-ics2023 <tracer@njuics.org>
2023-12-09 15:58:21 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:58:21 up 16:32,  3 users,  load average: 0.32, 0.18, 0.35

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -117,6 +117,8 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
 
   if (is_skip_ref) {
     // to skip the checking of an instruction, just copy the reg state to reference design
+    printf("skip_ref at pc = " FMT_WORD "\n", pc);
+    printf("cpu.pc = " FMT_WORD "\n", cpu.pc);
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
     is_skip_ref = false;
     return;
