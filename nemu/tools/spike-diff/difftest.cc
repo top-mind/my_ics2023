b5c2b0981eacd9226134b8dc2b08de064346b5a8
tracer-ics2023 <tracer@njuics.org>
2023-12-09 15:13:03 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:13:03 up 15:46,  3 users,  load average: 0.22, 0.30, 0.17

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -49,7 +49,7 @@ static state_t *state = NULL;
 void sim_t::diff_init(int port) {
   p = get_core("0");
   state = p->get_state();
-  std::cout << "mcause: " << state->mcause->read() << std::endl;
+  std::cout << "mstatus: " << std::hex << state->mstatus->read() << std::endl;
 }
 
 void sim_t::diff_step(uint64_t n) {
