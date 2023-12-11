bfa75b392fa2e65ef8706033adb4123952c1de75
tracer-ics2023 <tracer@njuics.org>
2023-12-12 06:52:52 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  06:52:52 up 13:07,  4 users,  load average: 0.24, 0.36, 0.30

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -70,13 +70,15 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  extern int end;
-  static intptr_t program_break = (intptr_t) &end;
-  if (_syscall_(SYS_brk, program_break + increment, 0, 0) == 0) {
-    intptr_t old_program_break = program_break;
-    program_break += increment;
-    return (void *)old_program_break;
-  }
+  extern int errno;
+  // extern int end;
+  // static intptr_t program_break = (intptr_t) &end;
+  // if (_syscall_(SYS_brk, program_break + increment, 0, 0) == 0) {
+  //   intptr_t old_program_break = program_break;
+  //   program_break += increment;
+  //   return (void *)old_program_break;
+  // }
+  errno = 12;
   return (void *)-1;
 }
 
