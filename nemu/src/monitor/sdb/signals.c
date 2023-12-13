fb4b0a063312ed354769042d2ab9275f673fe413
tracer-ics2023 <tracer@njuics.org>
2023-12-13 18:46:52 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:46:52 up 2 days,  1:01,  4 users,  load average: 0.20, 0.22, 0.30

--- nemu/src/monitor/sdb/signals.c
+++ nemu/src/monitor/sdb/signals.c
@@ -1,8 +1,14 @@
 #include <signal.h>
-#include <stddef.h>
+#include <common.h>
 
 int sync_sigint;
 
-void sigint_handler(int sig) { sync_sigint = 1; }
+void sigint_handler(int sig) { 
+  if (nemu_state.state == NEMU_RUNNING) {
+    sync_sigint = 1;
+  } else {
+    exit(0);
+  }
+}
 
 void init_sigint() { signal(SIGINT, sigint_handler); }
