d5a7bf2f8966af5946d502d467bc34be23147f95
tracer-ics2023 <tracer@njuics.org>
2023-12-14 00:08:35 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:08:35 up 2 days,  6:23,  4 users,  load average: 0.41, 0.35, 0.36

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -3,12 +3,12 @@
 #include <string.h>
 
 int main() {
-  // FILE *fp = fopen("/share/files/num", "r+");
-  // assert(fp);
+  FILE *fp = fopen("/share/files/num", "r+");
+  assert(fp);
 
-  // fseek(fp, 0, SEEK_END);
-  // long size = ftell(fp);
-  // assert(size == 5000);
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+  assert(size == 5000);
 
   // fseek(fp, 500 * 5, SEEK_SET);
   // int i, n;
