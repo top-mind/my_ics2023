44353080571242b1abc5f62326a7839ea335d05e
tracer-ics2023 <tracer@njuics.org>
2023-12-14 05:43:42 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:43:42 up 2 days, 11:58,  5 users,  load average: 0.31, 0.32, 0.26

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -3,10 +3,18 @@
 #include <unistd.h>
 
 int main() {
-  // struct timeval tv;
+  uint32_t timeus;
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  timeus = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+  int cnt = 0;
   while (1) {
-    // gettimeofday(&tv, NULL);
-    usleep(500000);
+    gettimeofday(&tv, NULL);
+    uint32_t timeus_cur = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+    if ((timeus_cur - timeus) / 1000 > cnt) {
+      printf("Hello World!\n");
+      cnt = (timeus_cur - timeus) / 1000;
+    }
   }
   return 0;
 }
