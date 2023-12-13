8a7637481b1549e44422c420a5bc6d4f54734e83
tracer-ics2023 <tracer@njuics.org>
2023-12-13 22:31:26 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:31:26 up 2 days,  4:46,  4 users,  load average: 0.48, 0.35, 0.23

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -28,6 +28,12 @@ void do_syscall(Context *c) {
     case SYS_read:
       c->GPRx = fs_read(a[1], (void *) a[2], a[3]);
       break;
+    case SYS_open:
+      c->GPRx = fs_open((char *) a[1], a[2], a[3]);
+      break;
+    case SYS_close:
+      c->GPRx = 0;
+      break;
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
