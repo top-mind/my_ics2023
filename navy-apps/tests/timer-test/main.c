1f61337a57aa8b6b959bd3f331e9e7ccb59775ce
tracer-ics2023 <tracer@njuics.org>
2023-12-14 05:52:33 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:52:33 up 2 days, 12:07,  5 users,  load average: 0.46, 0.47, 0.38

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <NDL.h>
 
 int main() {
   const int period = 2000;
-  uint32_t timeus;
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  timeus = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+  uint32_t timeus = NDL_GetTicks();
   int cnt = 0;
   while (1) {
-    gettimeofday(&tv, NULL);
-    uint32_t timeus_cur = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+    uint32_t timeus_cur = NDL_GetTicks();
     if ((timeus_cur - timeus) / period > cnt) {
       printf("Hello World!\n");
       cnt = (timeus_cur - timeus) / period;
