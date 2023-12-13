1b6cc0ba3593a6c434a945da06b0b0027b6f688f
tracer-ics2023 <tracer@njuics.org>
2023-12-14 03:54:28 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:54:28 up 2 days, 10:09,  5 users,  load average: 0.38, 0.36, 0.31

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -23,7 +23,7 @@
 
 static uint8_t *serial_base = NULL;
 
-static void serial_putc(char ch) { MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr)); }
+static void serial_putc(char ch) { MUXDEF(CONFIG_TARGET_AM, putch(ch), fputc(ch, stderr)); }
 
 static void serial_io_handler(uint32_t offset, int len, bool is_write) {
   assert(len == 1);
