0f1956472b3cb6134073f9c33fe3f522102602e9
tracer-ics2023 <tracer@njuics.org>
2023-12-13 22:30:34 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:30:34 up 2 days,  4:45,  4 users,  load average: 0.56, 0.33, 0.22

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,7 @@
 #include <common.h>
+#include <fs.h>
 #include "syscall.h"
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -17,19 +19,16 @@ void do_syscall(Context *c) {
       c->GPRx = 0;
       break;
     case SYS_write:
-      Log("Write syscall with fd = %d, buf = %p, len = %d", a[1], a[2], a[3]);
-      if (a[1] == 1 || a[1] == 2) {
-        for (size_t i = 0; i < a[3]; i++)
-          putch(*(char *)(a[2] + i));
-        c->GPRx = a[3];
-      } else {
-        c->GPRx = -1;
-      }
+      c->GPRx = fs_write(a[1], (void *) a[2], a[3]);
       break;
     case SYS_brk:
       Log("brk syscall with addr = %p", a[1]);
       c->GPRx = 0;
       break;
+    case SYS_read:
+      c->GPRx = fs_read(a[1], (void *) a[2], a[3]);
+      break;
+
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
