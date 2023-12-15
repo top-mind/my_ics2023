835a4bd8cf627af57539d2a687e79235d9aef578
tracer-ics2023 <tracer@njuics.org>
2023-12-15 20:51:30 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:51:30 up 4 days,  3:06,  4 users,  load average: 0.76, 0.55, 0.37

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -6,7 +6,7 @@
 int main() {
   NDL_Init(0);
   while (1) {
-    char buf[10];
+    char buf[11];
     int ret;
     if (0 != (ret =  NDL_PollEvent(buf, sizeof(buf)))) {
       printf("receive event: %s len = %d, %d\n", buf, strlen(buf), ret);
