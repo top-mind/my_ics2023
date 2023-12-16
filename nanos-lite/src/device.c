d41b538565317cde9394c74a4e5acc90e1747e6b
tracer-ics2023 <tracer@njuics.org>
2023-12-17 02:57:11 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:57:11 up  8:45,  4 users,  load average: 0.45, 0.64, 1.03

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -39,7 +39,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return snprintf(buf, len, "WIDTH: %d\nHEIGHT: %d\n", screen_w, screen_h);
+  return snprintf(buf, len, "WIDTH : %d\nHEIGHT : %d\n", screen_w, screen_h);
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
