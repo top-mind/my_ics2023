8baec663cbca0b4279421127c1ea5915e4ab4d5a
tracer-ics2023 <tracer@njuics.org>
2023-12-17 02:56:22 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:56:22 up  8:44,  4 users,  load average: 0.56, 0.68, 1.07

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -38,7 +38,7 @@ static Finfo file_table[] __attribute__((used)) = {
     [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
     [FD_FB] = {"/dev/fb", 0, 0, invalid_read, invalid_write},
     [FD_EVENT] = {"/dev/events", 0, 0, events_read, invalid_write},
-    [FD_DISPINFO] = {"/dev/dispinfo", 0, 0, dispinfo_read, invalid_write},
+    [FD_DISPINFO] = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
