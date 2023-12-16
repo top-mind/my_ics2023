392176594de07a2a32955590a4c4040e1a84bcd5
tracer-ics2023 <tracer@njuics.org>
2023-12-17 06:57:30 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  06:57:30 up 12:45,  4 users,  load average: 0.41, 0.54, 0.97

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -57,7 +57,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
     // slow path
     panic("Unaligned framebuffer write is not implemented");
   }
-  return 0;
+  return len;
 }
 
 int get_fbsize() {
