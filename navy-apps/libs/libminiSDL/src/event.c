a60970929597f9075c740b4287168a42890023d3
tracer-ics2023 <tracer@njuics.org>
2023-12-18 02:44:37 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:44:37 up 1 day,  8:32,  3 users,  load average: 0.49, 0.48, 0.82

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,7 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <assert.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -8,12 +10,29 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
+static inline uint8_t find_keyname(const char *name) {
+  for (int i = 0; i < sizeof(keyname) / sizeof(keyname[0]); i ++) {
+    if (strcmp(keyname[i], name) == 0) {
+      return i;
+    }
+  }
+  assert(0);
+  return 0;
+}
+
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+  assert(ev);
+  char buf[32];
+  int ret = NDL_PollEvent(buf, sizeof(buf));
+  if (ret) {
+    ev->type = buf[1] == 'u' ? SDL_KEYUP : SDL_KEYDOWN;
+    ev->key.keysym.sym = find_keyname(buf + 3);
+  }
+  return ret;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
