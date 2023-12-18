cf7de504743c522010832a0b2e9dc7eb1db917a3
tracer-ics2023 <tracer@njuics.org>
2023-12-19 01:35:15 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:35:15 up  7:43,  4 users,  load average: 0.40, 0.51, 0.46

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -12,7 +12,14 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  FILE *f = fopen(filename, "r");
+  long size = ftell(f);
+  void *buf = malloc(size);
+  fread(buf, size, 1, f);
+  SDL_Surface *ret = STBIMG_LoadFromMemory(buf, size);
+  free(buf);
+  fclose(f);
+  return ret;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
