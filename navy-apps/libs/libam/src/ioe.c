cdec7608aacc226194ad6283d6143f4f907a1bec
tracer-ics2023 <tracer@njuics.org>
2023-12-21 01:47:52 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:47:52 up 21 min,  4 users,  load average: 0.40, 0.43, 0.38

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -1,8 +1,13 @@
 #include <am.h>
+#include <assert.h>
 
 bool ioe_init() {
   return true;
 }
 
-void ioe_read (int reg, void *buf) { }
-void ioe_write(int reg, void *buf) { }
+void ioe_read (int reg, void *buf) {
+  assert(0);
+}
+void ioe_write(int reg, void *buf) {
+  assert(0);
+}
