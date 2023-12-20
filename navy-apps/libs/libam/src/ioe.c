8b519163b3191229340fc70aedbfba4fed2d801b
tracer-ics2023 <tracer@njuics.org>
2023-12-21 02:13:47 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:13:47 up 47 min,  5 users,  load average: 0.16, 0.30, 0.29

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -1,16 +1,29 @@
 #include <am.h>
 #include <assert.h>
+#include <NDL.h>
+
+#define concat_temp(x, y) x ## y
+#define concat(x, y) concat_temp(x, y)
+
+int w, h;
 
 bool ioe_init() {
+  NDL_Init(0);
+  NDL_OpenCanvas(&w, &h);
   return true;
 }
 
+#define IOE_QUICKSET(reg, buf, ...)                                            \
+  case reg: {                                                                  \
+    reg##_T _tmp = {__VA_ARGS__};                                              \
+    *(reg##_T *)buf = _tmp;                                                    \
+    break;                                                                     \
+  }
+
 void ioe_read (int reg, void *buf) {
-  printf("reg = %d\n", reg);
   switch (reg) {
-    case AM_TIMER_CONFIG:
-      ((AM_TIMER_CONFIG_T *)buf)->present = 1;
-      break;
+    IOE_QUICKSET(AM_TIMER_CONFIG, buf, .present = 1);
+    IOE_QUICKSET(AM_GPU_CONFIG, buf, .present = 1, .height = h, .width = w);
     default:
       fprintf(stderr, "Unknown ioe port %d\n", reg);
       assert(0);
