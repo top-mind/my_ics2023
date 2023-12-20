1a91d977b2778e69a4d6930e96b5061d0dfa2273
tracer-ics2023 <tracer@njuics.org>
2023-12-21 02:55:05 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:55:05 up  1:29,  5 users,  load average: 0.63, 0.45, 0.30

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -55,6 +55,7 @@ void ioe_read (int reg, void *buf) {
       }
       *((AM_INPUT_KEYBRD_T *)buf) =
           (AM_INPUT_KEYBRD_T){.keydown = keydown, .keycode = find};
+      printf("%d\n", find);
       break;
     }
     default:
