1862f877615a10db8d75c0332bbe1ea79396f9a3
tracer-ics2023 <tracer@njuics.org>
2023-12-23 22:13:14 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:13:14 up 2 days, 20:47,  4 users,  load average: 1.68, 1.70, 1.40

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -49,6 +49,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   void watchpoints_notify();
   watchpoints_notify();
 #endif
+  return;
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 }
 
