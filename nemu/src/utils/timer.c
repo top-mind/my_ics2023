db9a849543c757a8592aad6aac7ce3bc2b6d7e56
tracer-ics2023 <tracer@njuics.org>
2023-12-25 16:20:30 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:20:30 up 4 days, 14:54,  3 users,  load average: 0.62, 0.38, 0.25

--- nemu/src/utils/timer.c
+++ nemu/src/utils/timer.c
@@ -35,6 +35,7 @@ static uint64_t get_time_internal() {
   struct timeval now;
   gettimeofday(&now, NULL);
   uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
+  printf("%" PRIu64 "\n", us);
 #else
   struct timespec now;
   clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
