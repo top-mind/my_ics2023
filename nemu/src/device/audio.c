74370a13e578ca68b394f4f8a66a27d7bce6793f
tracer-ics2023 <tracer@njuics.org>
2023-12-29 09:32:13 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:32:13 up 23:00,  4 users,  load average: 0.23, 0.13, 0.29

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -67,6 +67,7 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
       } else {
         audio_base[reg_count] = PSEDOBUF_SIZE - used;
       }
+      printf("%d\n", audio_base[reg_count]);
       break;
     }
     case paddr_start_lo:
