0a595834436082fdd1b46d921c7c8ae710a8a2e0
tracer-ics2023 <tracer@njuics.org>
2023-12-29 09:57:18 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:57:18 up 23:25,  4 users,  load average: 0.72, 0.53, 0.49

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -38,7 +38,7 @@ static uint32_t *audio_base = NULL;
 #define PSEDOBUF_SIZE 0x10000
 
 #if 1
-#define DELAY_NR 10
+#define DELAY_NR 5
 static int delay_count = 0;
 static int old_used = 0;
 #endif
@@ -68,8 +68,8 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
     case reg_count: {
       int used = SDL_GetQueuedAudioSize(1);
       if (old_used < used || delay_count == DELAY_NR) {
-          delay_count = 0;
-          old_used = used;
+        delay_count = 0;
+        old_used = used;
       } else {
         delay_count++;
         used = old_used;
