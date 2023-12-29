50fbdf65ac85599eb5b613913f32e4a9dec84575
tracer-ics2023 <tracer@njuics.org>
2023-12-29 09:02:13 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:02:13 up 22:30,  4 users,  load average: 0.73, 0.70, 0.88

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -69,9 +69,8 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
     {
       paddr_t start = *(paddr_t *) &audio_base[paddr_start_lo];
       paddr_t end = *(paddr_t *) &audio_base[paddr_end_lo];
-      printf("audio: start = %x, end = %x, len = %x\n", start, end, start - end);
-      fflush(stdout);
-      // SDL_QueueAudio(1, guest_to_host(0x8000000), 0x10);
+      // printf("audio: start = %x, end = %x, len = %x\n", start, end, start - end);
+      SDL_QueueAudio(1, guest_to_host(start), end - start);
       break;
     }
     default:
