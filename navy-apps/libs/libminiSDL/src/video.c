38ef86e3ac673827cb06b95791acf822a99e7a3c
tracer-ics2023 <tracer@njuics.org>
2023-12-30 02:55:36 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:55:36 up  7:16,  4 users,  load average: 1.02, 0.97, 0.60

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -1,3 +1,4 @@
+#include "sdl-audio.h"
 #include <NDL.h>
 #include <SDL.h>
 #include <assert.h>
@@ -79,6 +80,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
+  InvokeAudioCallback();
   assert(s->format->BitsPerPixel == 8 || s->format->BitsPerPixel == 32);
   if ((x | y | w | h) == 0) {
     w = s->w;
