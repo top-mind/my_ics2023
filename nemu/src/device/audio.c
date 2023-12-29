6c2192f222c6a1ee2aa1e28a9fbb7fed128ea873
tracer-ics2023 <tracer@njuics.org>
2023-12-29 09:44:48 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:44:48 up 23:13,  4 users,  load average: 1.73, 0.88, 0.55

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -35,7 +35,7 @@ enum {
 
 static uint32_t *audio_base = NULL;
 
-#define PSEDOBUF_SIZE 0x10
+#define PSEDOBUF_SIZE 0x100
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
   switch(offset / 4) {
