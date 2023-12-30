9a56f74372bf4ecfe981d340c9e68e5bc10746ef
tracer-ics2023 <tracer@njuics.org>
2023-12-30 15:15:07 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:15:07 up 19:36,  4 users,  load average: 0.49, 0.26, 0.23

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -1,6 +1,7 @@
 #include <NDL.h>
 #include <SDL.h>
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -47,13 +48,68 @@ void SDL_PauseAudio(int pause_on) {
 }
 
 void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {
+  int16_t *d = (int16_t *)dst, *s = (int16_t *)src;
+  int i;
+  for (i = 0; i < len / 2; i++) {
+    int32_t t = d[i] + s[i] * volume / SDL_MIX_MAXVOLUME;
+    if (t > 32767) t = 32767;
+    if (t < -32768) t = -32768;
+    d[i] = t;
+  }
 }
 
+
 SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audio_buf, uint32_t *audio_len) {
-  return NULL;
+  if (file == NULL || spec == NULL || audio_buf == NULL || audio_len == NULL)
+    return NULL;
+  FILE *f = fopen(file, "r");
+  if (!f)
+    return NULL;
+  struct WAVEHeader {
+    char riff[4];
+    uint32_t size;
+    char wave[4];
+    char fmt[4];
+    uint32_t fmt_size;
+    struct {
+      uint16_t format;
+      uint16_t channels;
+      uint32_t sample_rate;
+      uint32_t byte_rate;
+      uint16_t block_align;
+      uint16_t bits_per_sample;
+    } fmt_data;
+    char data[4];
+    uint32_t data_size;
+  } header;
+  if (fread(&header, 1, sizeof header, f) != sizeof header) {
+    fclose(f);
+    return NULL;
+  }
+  if (memcmp(header.riff, "RIFF", 4) || memcmp(header.wave, "WAVE", 4) ||
+      memcmp(header.fmt, "fmt ", 4) || memcmp(header.data, "data", 4) ||
+      header.fmt_data.format != 1 || header.fmt_data.bits_per_sample != 16) {
+    fclose(f);
+    return NULL;
+  }
+  memset(spec, 0, sizeof *spec);
+  spec->freq = header.fmt_data.sample_rate;
+  spec->format = AUDIO_S16SYS;
+  spec->channels = header.fmt_data.channels;
+  spec->samples = 1024;
+
+  *audio_len = header.data_size;
+  *audio_buf = malloc(header.data_size);
+  if (fread(*audio_buf, 1, header.data_size, f) != header.data_size) {
+    fclose(f);
+    return NULL;
+  }
+  fclose(f);
+  return spec;
 }
 
 void SDL_FreeWAV(uint8_t *audio_buf) {
+  free(audio_buf);
 }
 
 void SDL_LockAudio() {
