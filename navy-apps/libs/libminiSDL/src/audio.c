449ba443d6fe0b19f24264b6735d33e45d86be77
tracer-ics2023 <tracer@njuics.org>
2023-12-30 13:36:39 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:36:39 up 17:57,  4 users,  load average: 0.91, 0.80, 0.46

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -31,9 +31,9 @@ int SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained) {
     desired = obtained;
   }
   desired->size = callback_size =
-      desired->samples * desired->channels * sizeof(uint16_t) * 2;
+      desired->samples * desired->channels * sizeof(uint16_t);
   audio_buf = malloc(callback_size);
-  callback_period = 1000 * desired->samples / desired->freq * 2;
+  callback_period = 1000 * desired->samples / desired->freq;
   return 0;
 }
 
