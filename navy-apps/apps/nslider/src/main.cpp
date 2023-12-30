86f3f2527704f942b84b216e0d42c11a2b78fef2
tracer-ics2023 <tracer@njuics.org>
2023-12-30 15:38:38 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:38:38 up 19:59,  4 users,  load average: 0.76, 0.57, 0.47

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -57,6 +57,7 @@ int main() {
 
     if (e.type == SDL_KEYDOWN) {
       switch(e.key.keysym.sym) {
+        case SDLK_ESCAPE: return 0;
         case SDLK_0: rep = rep * 10 + 0; break;
         case SDLK_1: rep = rep * 10 + 1; break;
         case SDLK_2: rep = rep * 10 + 2; break;
