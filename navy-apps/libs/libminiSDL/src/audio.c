cd5b6596049655b48a62da8e90b78aa821ed07d6
tracer-ics2023 <tracer@njuics.org>
2024-01-08 11:54:11 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:54:11 up 23:34,  4 users,  load average: 0.71, 0.70, 0.58

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -58,7 +58,6 @@ void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {
   }
 }
 
-
 SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audio_buf, uint32_t *audio_len) {
   if (file == NULL || spec == NULL || audio_buf == NULL || audio_len == NULL)
     return NULL;
