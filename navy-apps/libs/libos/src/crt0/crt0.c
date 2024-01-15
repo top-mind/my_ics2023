b5f3e4fe97c0a03829c3efa8d93c038957fed29f
tracer-ics2023 <tracer@njuics.org>
2024-01-16 03:59:47 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:59:47 up 2 days,  6:03,  5 users,  load average: 0.26, 0.37, 0.36

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
@@ -8,6 +9,8 @@ void __libc_init_array (void);
 void call_main(uintptr_t *args) {
   int argc = (int)args[0];
   char **argv = (char **)args + 1;
+  printf("%p %p\n", args, argv);
+  while (1);
   environ = (char **)args + argc + 2;;
   __libc_init_array();
   exit(main(argc, argv, environ));
