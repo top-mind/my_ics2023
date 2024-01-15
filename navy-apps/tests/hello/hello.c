3c201baea82300c93ea8de1fd29458a8a26b1436
tracer-ics2023 <tracer@njuics.org>
2024-01-16 05:48:22 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:48:22 up 2 days,  7:52,  5 users,  load average: 0.43, 0.23, 0.10

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -7,7 +7,7 @@
 void * b;
 
 int main(int argc, char *argv[]) {
-  printf("%d %s %p\n", argc, argv[0], argv[1]);
+  printf("%d %s %s\n", argc, argv[0], argv[1]);
   while (1);
   return 0;
   write(1, "Hello World!\n", 13);
