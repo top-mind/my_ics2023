299d580a399e19f98199b604f2391ca59aba248f
tracer-ics2023 <tracer@njuics.org>
2024-01-16 09:19:32 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:19:32 up 2 days, 11:23,  5 users,  load average: 0.43, 0.50, 0.45

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -3,17 +3,26 @@
 #include <fcntl.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 void * b;
 
-char *exe_argv[] = {NULL};
+const struct option long_options[] = {
+  {"skip", 0, NULL, 'b'},
+  {NULL, 0, NULL, 0}
+};
 
 int main(int argc, char *argv[], char *envp[]) {
-  printf("Hello World!\n");
-  printf("argc = %d\n", argc);
-  for (int i = 0; i < argc; i ++) {
-    printf("argv[%d] = %s\n", i, argv[i]);
+  // printf("Hello World!\n");
+  // printf("argc = %d\n", argc);
+  // for (int i = 0; i < argc; i ++) {
+  //   printf("argv[%d] = %s\n", i, argv[i]);
+  // }
+  while (1) {
+    int opt = getopt_long_only(argc, argv, "", long_options, NULL);
+    if (opt == -1) break;
   }
+  while (1);
   return 0;
   write(1, "Hello World!\n", 13);
   int i = 2;
