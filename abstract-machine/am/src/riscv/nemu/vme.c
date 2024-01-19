546719890448a2e24825a2d14b82d822302c2e6a
tracer-ics2023 <tracer@njuics.org>
2024-01-19 14:53:54 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:53:54 up 5 days, 16:57,  4 users,  load average: 0.38, 0.54, 0.45

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -61,7 +61,7 @@ void __am_get_cur_as(Context *c) {
 }
 
 void __am_switch(Context *c) {
-  if (vme_enable && c->pdir != NULL) {
+  if (vme_enable && c->pdir != NULL && c->pdir != kas.ptr) {
     set_satp(c->pdir);
   }
 }
