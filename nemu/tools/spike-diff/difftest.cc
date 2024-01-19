a58a43141edccbfb042dbd6fa40d1cd4d94b9ca5
tracer-ics2023 <tracer@njuics.org>
2024-01-19 16:36:08 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:08 up 5 days, 18:40,  3 users,  load average: 1.02, 0.74, 0.56

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -80,6 +80,7 @@ void sim_t::diff_set_regs(void* diff_context) {
   state->mepc->write(ctx->mepc);
   state->mtvec->write(ctx->mtvec);
   state->satp->write(ctx->satp);
+  printf("%d\n", ctx->prv);
   state->prv = ctx->prv;
 }
 
