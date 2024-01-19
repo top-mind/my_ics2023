81ab0c6931c528d1f367467acda29c2cb55e6540
tracer-ics2023 <tracer@njuics.org>
2024-01-19 14:49:36 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:49:36 up 5 days, 16:53,  4 users,  load average: 0.57, 0.67, 0.47

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,8 +8,7 @@ void __am_get_cur_as(Context *c);
 void __am_switch(Context *c);
 
 Context *__am_irq_handle(Context *c) {
-  if (c->pdir != NULL)
-    __am_get_cur_as(c);
+  __am_get_cur_as(c);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
