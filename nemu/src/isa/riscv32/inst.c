daa47a1f723cfc67daf0af0b7b58cdac991b6174
tracer-ics2023 <tracer@njuics.org>
2024-01-21 03:21:00 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:21:00 up 7 days,  5:25,  5 users,  load average: 0.38, 0.45, 0.47

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -234,8 +234,11 @@ static int decode_exec(Decode *s) {
           cpu.prv = cpu.mpp, cpu.mpp = PRV_LOW, cpu.mie = cpu.mpie, cpu.mpie = 1);
 
   // 0x6b = 0b1101011
-  INSTPAT("0000000 00000 00000 000 00000 11010 11", trap, N, printf("0x6b trap: a0 = %d\n", R(10))
-      , nemu_state.state = NEMU_STOP);
+  INSTPAT("0000000 00000 00000 000 00000 11010 11", trap, N, printf("0x6b trap: a0 = %d\n", R(10)),
+          nemu_state.state = NEMU_STOP);
+  // 0b11101011 = 0xeb
+  INSTPAT("0000000 00000 00000 000 00001 11010 11", trap, N, printf("0xeb trap: a0 = %d\n", R(10)),
+          nemu_state.state = NEMU_STOP);
   //
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv, N, INV(s->pc));
   INSTPAT_END();
