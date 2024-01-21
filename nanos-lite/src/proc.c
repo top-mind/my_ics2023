b2348c87913df6bfed65d6ac21c9ca476a2c0a1a
tracer-ics2023 <tracer@njuics.org>
2024-01-22 04:42:34 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:42:34 up 8 days,  6:46,  4 users,  load average: 0.22, 0.91, 1.17

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,7 +22,8 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   Log("Initializing processes...");
-  context_kload(&pcb[0], (void *)hello_fun, (void *)0x12345678);
+  context_uload(&pcb[0], "/bin/nterm", (char *const[]){"/bin/nterm", NULL}, (char *const[]){NULL});
+  // context_kload(&pcb[0], (void *)hello_fun, (void *)0x12345678);
   assert(pcb[0].cp);
   context_uload(&pcb[1], "/bin/nterm", (char *const[]){"/bin/nterm", NULL}, (char *const[]){NULL});
   // context_uload(&pcb[1], "/bin/pal", (char *const[]){"/bin/pal", "--skip", NULL}, (char *const[]){NULL});
