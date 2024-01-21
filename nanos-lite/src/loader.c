c7828d906b95fdc4b78e46ab585bf66453c139c5
tracer-ics2023 <tracer@njuics.org>
2024-01-22 03:19:18 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:19:18 up 8 days,  5:23,  4 users,  load average: 1.10, 0.87, 0.64

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -39,7 +39,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   int fd = fs_open(filename, 0, 0);
   if(fd < 0) {
     nanos_errno = ENOENT;
-    Log("file '%s' line%d", filename, __LINE__);
+    Log("fs_open '%s' return with %d", fd);
     goto out;
   }
   fs_read(fd, &ehdr, sizeof ehdr);
@@ -48,7 +48,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       || ehdr.e_machine != EXPECT_VALUE
       || ehdr.e_version != EV_CURRENT) {
     nanos_errno = ENOEXEC;
-    Log("file '%s' line%d", filename, __LINE__);
     goto out;
   }
   uint32_t nr_ph = ehdr.e_phnum;
@@ -61,7 +60,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   if (nr_ph * sizeof(Elf_Phdr) > PGSIZE) {
     Log("Program header size too large (nr_ph = %d, file = %s)", nr_ph, filename);
     nanos_errno = ENOEXEC;
-    Log("file '%s' line%d", filename, __LINE__);
     goto out;
   }
   Elf_Phdr *phdr = (Elf_Phdr *)new_page(1);
@@ -101,9 +99,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   return ehdr.e_entry;
 out_free:
   free_page(phdr);
-  Log("file '%s' fail1", filename);
 out:
-  Log("file '%s' fail2", filename);
   return 0;
 }
 
