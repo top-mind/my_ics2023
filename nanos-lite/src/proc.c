8f3f99a600c243d5dd4276d151a329d60d7bbfcc
tracer-ics2023 <tracer@njuics.org>
2024-01-22 18:54:39 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:54:39 up 8 days, 20:58,  3 users,  load average: 0.18, 0.38, 0.27

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,17 +24,19 @@ void init_proc() {
   Log("Initializing processes...");
   context_uload(&pcb[0], "/bin/hello", (char *const[]){"hello", NULL}, (char *const[]){NULL});
   context_uload(&pcb[1], "/bin/nterm", (char *const[]){"/bin/nterm", NULL}, (char *const[]){NULL});
-  // context_uload(&pcb[1], "/bin/pal", (char *const[]){"/bin/pal", "--skip", NULL}, (char *const[]){NULL});
-  // context_uload(&pcb[1], "/bin/nterm", (char *const[]){"/bin/nterm", NULL}, (char *const[]){NULL});
+  context_uload(&pcb[2], "/bin/pal", (char *const[]){"/bin/pal", "--skip", NULL}, (char *const[]){NULL});
+  context_uload(&pcb[3], "/bin/nterm", (char *const[]){"/bin/nterm", NULL}, (char *const[]){NULL});
   switch_boot_pcb();
 }
+
+PCB *fg_pcb = &pcb[1];
 
 Context* schedule(Context *prev) {
   // Log("prev: %p", prev);
   current->cp = prev;
   // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   if (current == &pcb[0]) {
-    current = &pcb[1];
+    current = fg_pcb;
   } else {
     current = &pcb[0];
   }
