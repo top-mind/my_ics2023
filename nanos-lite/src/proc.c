e34868cd2fca334319c3bca3263f13a8bb56a005
tracer-ics2023 <tracer@njuics.org>
2024-01-22 08:45:34 +0800

    >  compile NEMU 221900006 耿天成 Linux ThinkPad-E480 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:45:34 up 8 days, 10:49,  3 users,  load average: 0.33, 0.43, 0.36

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,10 +22,8 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   Log("Initializing processes...");
-  context_kload(&pcb[0], (void *)hello_fun, (void *)0x12345678);
-  assert(pcb[0].cp);
-  context_uload(&pcb[1], "/bin/nterm", (char *const[]){"/bin/nterm", NULL}, (char *const[]){NULL});
-  assert(pcb[1].cp);
+  context_kload(&pcb[1], (void *)hello_fun, (void *)0x12345678);
+  context_uload(&pcb[0], "/bin/nterm", (char *const[]){"/bin/nterm", NULL}, (char *const[]){NULL});
   // context_uload(&pcb[1], "/bin/pal", (char *const[]){"/bin/pal", "--skip", NULL}, (char *const[]){NULL});
   // context_uload(&pcb[1], "/bin/nterm", (char *const[]){"/bin/nterm", NULL}, (char *const[]){NULL});
   // context_uload(&pcb[1], "/bin/hello", (char *const[]){"/bin/hello", "-s", NULL}, (char *const[]){"a=x", "b=y", NULL});
